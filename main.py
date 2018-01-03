ab7eb7055df31718d83224b2fa365106c80f7e06
MrColT <coltredstone@gmail.com>
2018-01-03 15:45:21 +0600

    del trash

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import requests
-import csv
 import lxml.html as lxmlhtml
-from datetime import datetime
-from multiprocessing import Pool
 
 headers = {
     'user-agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.108 Safari/537.36'
