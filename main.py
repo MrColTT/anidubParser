1258c5f90a7ea6279a543d44aa2f649046fd9879
MrColT <coltredstone@gmail.com>
2018-01-04 14:40:57 +0300

    todo number one is completed (nearly)

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
-import requests
+import aiohttp
+import asyncio
+import async_timeout
 import lxml.html as lxmlhtml
 
-headers = {
-    'user-agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.108 Safari/537.36'
+HEADERS = {
+    'user-agent': ('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.108 Safari/537.36'),
 }
-
-def get_html(url):
-    r = requests.get (url, headers=headers)
-    return r.text
 
 def get_pages(html):
     htmlToLXML = lxmlhtml.fromstring(html)
@@ -21,18 +19,21 @@ def get_titles(htmlData):
     titleResult = dict(list(zip(title, titleScore)))
     return titleResult
 
-def main():
-    url = 'https://online.anidub.com/anime_movie/'
-    htmlData = get_html(url)
-    # titlesList = get_titles(htmlData)
-    all_pages = get_pages(htmlData)
-    for i in range(1, all_pages + 1):
-        nextPage = get_html('https://online.anidub.com/anime_movie/page/'+str(i)+'/')
-        titlesList = get_titles(nextPage)
-        for x in titlesList:
-            print(x + ' ' + titlesList[x] + ' parsed')
-    
+async def fetch(session, url):
+    with async_timeout.timeout(10):
+        async with session.get(url, headers=HEADERS) as response:
+            return await response.text()
 
+async def main():
+    async with aiohttp.ClientSession() as session:
+        html = await fetch(session, 'https://online.anidub.com/anime_movie/')
+        all_pages = get_pages(html)
+        for i in range(1, all_pages + 1):
+            # nextPage = get_html('https://online.anidub.com/anime_movie/page/'+str(i)+'/')
+            nextPage = await fetch(session, 'https://online.anidub.com/anime_movie/page/'+str(i)+'/')
+            titlesList = get_titles(nextPage)
+            for x in titlesList:
+                print(x + ' ' + titlesList[x] + ' parsed')
 
-if __name__ == '__main__':
-    main ()
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
